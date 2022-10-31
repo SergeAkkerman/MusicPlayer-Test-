a59adbaba3fb7b8bdaead999418f8ebb45b33b60
sergey <sergey@localhost>
2022-10-31 12:13:53 +0200

    Dependency injection using XML configuration

--- src/main/java/en/serge/springcourse/TestSpring.java
+++ src/main/java/en/serge/springcourse/TestSpring.java
@@ -7,10 +7,11 @@ public class TestSpring {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
-		Music music = context.getBean("music", Music.class);
+		//Music music = context.getBean("music", Music.class);
+		//MusicPlayer musicPlayer = new MusicPlayer(music);
 		
-		MusicPlayer musicPlayer = new MusicPlayer(music);
 		
+		MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 		musicPlayer.playMusic();
 		
 		context.close();
